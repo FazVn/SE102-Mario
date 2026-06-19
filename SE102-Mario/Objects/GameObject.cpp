a286067fb01c0927357e9e0b20b04fd8f01daed8
Lê Nguyễn Thái Bình <chuanchauau@gmail.com>
2026-06-19 17:29:31 +0700

    Complete GameObject.h and GameObject.cpp

--- SE102-Mario/Objects/GameObject.cpp
+++ SE102-Mario/Objects/GameObject.cpp
@@ -1,5 +1,141 @@
 #include "GameObject.h"
 
-void GameObject::Update(float)
+float RectF::Width() const
 {
+    return right - left;
+}
+
+float RectF::Height() const
+{
+    return bottom - top;
+}
+
+bool RectF::Intersects(const RectF& other) const
+{
+    return left < other.right &&
+        right > other.left &&
+        top < other.bottom &&
+        bottom > other.top;
+}
+
+GameObject::GameObject(float x, float y, float width, float height)
+    : x(x), y(y), width(width), height(height)
+{
+}
+
+void GameObject::Update(float deltaTime)
+{
+    x += velocityX * deltaTime;
+    y += velocityY * deltaTime;
+}
+
+void GameObject::Render(Renderer&)
+{
+    // Base object does not know how it should look.
+    // Derived classes such as Mario, Brick, Platform, ... will override this.
+}
+
+void GameObject::OnCollision(GameObject&)
+{
+    // Default collision reaction: do nothing.
+    // Derived classes can override this when needed.
+}
+
+float GameObject::GetX() const
+{
+    return x;
+}
+
+float GameObject::GetY() const
+{
+    return y;
+}
+
+float GameObject::GetWidth() const
+{
+    return width;
+}
+
+float GameObject::GetHeight() const
+{
+    return height;
+}
+
+float GameObject::GetVelocityX() const
+{
+    return velocityX;
+}
+
+float GameObject::GetVelocityY() const
+{
+    return velocityY;
+}
+
+void GameObject::SetPosition(float newX, float newY)
+{
+    x = newX;
+    y = newY;
+}
+
+void GameObject::SetSize(float newWidth, float newHeight)
+{
+    width = newWidth < 0.0f ? 0.0f : newWidth;
+    height = newHeight < 0.0f ? 0.0f : newHeight;
+}
+
+void GameObject::SetVelocity(float newVelocityX, float newVelocityY)
+{
+    velocityX = newVelocityX;
+    velocityY = newVelocityY;
+}
+
+void GameObject::Move(float deltaX, float deltaY)
+{
+    x += deltaX;
+    y += deltaY;
+}
+
+RectF GameObject::GetBoundingBox() const
+{
+    return RectF{ x, y, x + width, y + height };
+}
+
+bool GameObject::IsActive() const
+{
+    return active;
+}
+
+bool GameObject::IsVisible() const
+{
+    return active && visible;
+}
+
+bool GameObject::IsCollidable() const
+{
+    return active && collidable;
+}
+
+bool GameObject::IsSolid() const
+{
+    return active && collidable && solid;
+}
+
+void GameObject::SetActive(bool value)
+{
+    active = value;
+}
+
+void GameObject::SetVisible(bool value)
+{
+    visible = value;
+}
+
+void GameObject::SetCollidable(bool value)
+{
+    collidable = value;
+}
+
+void GameObject::SetSolid(bool value)
+{
+    solid = value;
 }
--- SE102-Mario/Objects/GameObject.h
+++ SE102-Mario/Objects/GameObject.h
@@ -1,8 +1,65 @@
 #pragma once
 
+class Renderer;
+
+struct RectF
+{
+    float left = 0.0f;
+    float top = 0.0f;
+    float right = 0.0f;
+    float bottom = 0.0f;
+
+    float Width() const;
+    float Height() const;
+    bool Intersects(const RectF& other) const;
+};
+
 class GameObject
 {
 public:
+    GameObject() = default;
+    GameObject(float x, float y, float width, float height);
     virtual ~GameObject() = default;
+
     virtual void Update(float deltaTime);
+    virtual void Render(Renderer& renderer);
+    virtual void OnCollision(GameObject& other);
+
+    float GetX() const;
+    float GetY() const;
+    float GetWidth() const;
+    float GetHeight() const;
+    float GetVelocityX() const;
+    float GetVelocityY() const;
+
+    void SetPosition(float newX, float newY);
+    void SetSize(float newWidth, float newHeight);
+    void SetVelocity(float newVelocityX, float newVelocityY);
+    void Move(float deltaX, float deltaY);
+
+    RectF GetBoundingBox() const;
+
+    bool IsActive() const;
+    bool IsVisible() const;
+    bool IsCollidable() const;
+    bool IsSolid() const;
+
+    void SetActive(bool value);
+    void SetVisible(bool value);
+    void SetCollidable(bool value);
+    void SetSolid(bool value);
+
+protected:
+    float x = 0.0f;
+    float y = 0.0f;
+    float width = 0.0f;
+    float height = 0.0f;
+
+    float velocityX = 0.0f;
+    float velocityY = 0.0f;
+
+    bool active = true;
+    bool visible = true;
+    bool collidable = true;
+    bool solid = false;
 };
